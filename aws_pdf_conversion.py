441ca240441f3eaf1dd8a2e87bb6c800dc823dc8
Padmanabhan K <padmanabhanvj9@gmail.com>
2019-03-02 12:09:03 +0530

    Update aws_pdf_conversion.py

--- aws_pdf_conversion.py
+++ aws_pdf_conversion.py
@@ -78,8 +78,7 @@ def index():
    print ('It worked!')
    print("https://s3.amazonaws.com/image-upload-rekognition/pdf_converson.pdf")
    #return(pdf.output('pdf_converson.pdf'))
-   return(json.dumps({'Status': 'Success', 'StatusCode': '200',
-                       'ReturnValue': "https://s3.amazonaws.com/image-upload-rekognition/pdf_converson.pdf  ,'ReturnCode':'RRTS'},indent=4))
+   return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue': "https://s3.amazonaws.com/image-upload-rekognition/pdf_converson.pdf"  ,'ReturnCode':'RRTS'},indent=4))
    
 
 print("hihlo")
